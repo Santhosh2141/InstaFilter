4889e390e208460df3ab03baa96e8b8d9dbcf99f
Santhosh2141 <snthshsrinivas@gmail.com>
2025-09-15 19:38:39 +0530

    Initial Commit

---
+++ InstaFilter/InstaFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaFilterApp.swift
+//  InstaFilter
+//
+//  Created by Santhosh Srinivas on 15/09/25.
+//
+
+import SwiftUI
+
+@main
+struct InstaFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
